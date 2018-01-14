6698fb56069f715b38e59c9bf9c6a4eb8536e616
Frank <frank.x@aliyun.com>
2018-01-14 20:05:22 +0800

    add repl

---
+++ repl/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/garslo/go-cmd-repl"
+	"github.com/garslo/go-cmd-repl/repl"
+)
+
+func Echo(args ...cmd_repl.Argument) interface{} {
+	echo := make([]string, len(args))
+	for i, arg := range args {
+		s, _ := arg.AsString()
+		echo[i] = strings.Trim(s, "\n")
+	}
+	return strings.Join(echo, " ")
+}
+
+func main() {
+	r := repl.New()
+	r.RegisterCommand("echo", Echo)
+	go r.Start()
+
+	// Do other things
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Kill, os.Interrupt)
+	<-quit
+}
